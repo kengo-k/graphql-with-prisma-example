650c6102c82c95c570ea331f954b5e690d661abc
kengo-k <kurobanekengo@gmail.com>
2022-09-21 22:32:43 +0900

    prettier

--- app/sample/pages/api/graphql.ts
+++ app/sample/pages/api/graphql.ts
@@ -1,41 +1,41 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { ApolloServer } from "apollo-server-micro";
-import { readFileSync } from "fs";
-import { join } from "path";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ApolloServer } from 'apollo-server-micro'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
-import { Resolvers } from "../../graphql/dist/generated-server";
+import { Resolvers } from '../../graphql/dist/generated-server'
 
-const path = join(process.cwd(), "graphql", "schema.graphql");
-const typeDefs = readFileSync(path).toString("utf-8");
+const path = join(process.cwd(), 'graphql', 'schema.graphql')
+const typeDefs = readFileSync(path).toString('utf-8')
 
 const users = [
-  { id: "1", name: "Alice" },
-  { id: "2", name: "Bob" },
-  { id: "3", name: "Carol" },
-];
+  { id: '1', name: 'Alice' },
+  { id: '2', name: 'Bob' },
+  { id: '3', name: 'Carol' },
+]
 
 const resolvers: Resolvers = {
   Query: {
     users: () => users,
   },
-};
+}
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
 
-const startServer = apolloServer.start();
+const startServer = apolloServer.start()
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await startServer;
+  await startServer
   await apolloServer.createHandler({
-    path: "/api/graphql",
-  })(req, res);
+    path: '/api/graphql',
+  })(req, res)
 }
 
 export const config = {
   api: {
     bodyParser: false,
   },
-};
+}
