28d10cd60ed5a5711534b6d6e27693ecfccad381
kengo-k <kurobanekengo@gmail.com>
2022-10-02 23:06:13 +0900

    migrate実行

---
+++ app/sample/prisma/migrations/20221002140407_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "priority" INTEGER NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
