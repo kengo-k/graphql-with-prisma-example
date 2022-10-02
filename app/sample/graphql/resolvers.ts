5c863bd952dce0df43992e849fd73a78570f632c
kengo-k <kurobanekengo@gmail.com>
2022-10-03 00:08:21 +0900

    固定値を返すresolverからprismaでDBの値を読むように修正

--- app/sample/graphql/resolvers.ts
+++ app/sample/graphql/resolvers.ts
@@ -1,9 +1,7 @@
 export const resolvers = {
   Query: {
-    tasks: () => {
-      return [
-        { id: 1, title: 'test_title', category: 'test_category', done: false },
-      ]
+    tasks: (_, args, ctx) => {
+      return ctx.prisma.task.findMany()
     },
   },
 }
